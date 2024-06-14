9517a5fe82e5a0a0b6e78c200925bf6767ddaca6
Navjit-Kaur-TX <132881216+Navjit-Kaur-TX@users.noreply.github.com>
2024-06-14 19:25:53 +0530

    Update backend.tf

--- modules/backend.tf
+++ modules/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "remote" {
         hostname = "gurpreet1.jfrog.io"
         organization = "tfstate"
-        token = "cmVmdGtuOjAxOjE3NDk4OTczMjE6cUxZWnppWldzRFFLVlJkbTk3QkVwNTE1S0Fj"
+        token = "<token>"
         workspaces {
           name = "dev"
             # prefix = "my-prefix-"
