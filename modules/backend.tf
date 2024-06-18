7297cf23c41c3a2428e76fedf6a43cd52d54e728
Navjit-Kaur-TX <132881216+Navjit-Kaur-TX@users.noreply.github.com>
2024-06-18 16:01:25 +0530

    Update backend.tf

--- modules/backend.tf
+++ modules/backend.tf
@@ -1,11 +1,8 @@
-variable "backend_token" {
-  type        = string
-  description = "Token for the Terraform backend"
-}
 terraform {
     backend "remote" {
         hostname = "gurpreet1.jfrog.io"
         organization = "tf2"
+        token = var.backend_token
         workspaces {
             prefix = "my-prefix-"
         }
